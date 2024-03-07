c931e7a62b1c03091206516820fbfbb5675057e0
Gustavo Monteiro <gustavo.monteiro@sou.inteli.edu.br>
2024-03-07 10:40:42 -0300

    Código_Adicionado

---
+++ GrupoGaGu.PonderadaBackend.WebApi/GrupoGaGu.PonderadaBackend.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace GrupoGaGu.PonderadaBackend.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
